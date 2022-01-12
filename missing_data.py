938cd6a9181eaea8f3e571ebfef7a028bbe1a012
icebrayilov <97582501+icebrayilov@users.noreply.github.com>
2022-01-12 10:39:29 +0400

    Update missing_data.py

--- missing_data.py
+++ missing_data.py
@@ -8,7 +8,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 2].values
 
 
 # Taking care of missing data
@@ -17,5 +17,5 @@ y = dataset.iloc[:, 3].values
 
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0) 
-imputer = imputer.fit(X[:, 1:3]) 
-X[:, 1:3] = imputer.transform(X[:, 1:3]) 
+imputer = imputer.fit(X[:, 0:2]) 
+X[:, 0:2] = imputer.transform(X[:, 0:2]) 
